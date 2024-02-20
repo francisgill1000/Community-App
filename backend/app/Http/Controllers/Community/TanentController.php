420f789bcb7ff91246a240bf22430258bd516b2c
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-02-20 14:18:41 +0400

    added

--- backend/app/Http/Controllers/Community/TanentController.php
+++ backend/app/Http/Controllers/Community/TanentController.php
@@ -372,6 +372,7 @@ class TanentController extends Controller
     public function getMemberTypes()
     {
         return [
+            'Owner',
             'Primary',
             'Family Member',
             'Relative',
