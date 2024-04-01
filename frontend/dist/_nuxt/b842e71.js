80001d27f3ef7c9904e9be2ee82f6f0e64c7e6c9
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-04-01 11:51:31 +0400

    added new build

---
+++ frontend/dist/_nuxt/b842e71.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[405],{1693:function(e,n,l){"use strict";l.r(n);var t={middleware:"userType"},r=l(116),component=Object(r.a)(t,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
